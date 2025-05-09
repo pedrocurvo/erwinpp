591e1e5393fdff4d73e7ea3b843c0185f98610d5
pedrocurvo <pedro.mpcurvo@gmail.com>
2025-05-09 18:23:02 +0200

    Add SLURM job scripts for environment setup and benchmarking
    - Introduced `setup_conda.sh` for setting up the Anaconda environment and installing the Erwin package.
    - Added `velocity_benchmark.sh` for executing the velocity benchmark with SLURM job configuration.
    - Both scripts include necessary module loads and job specifications for GPU usage.

---
+++ scripts/velocity_benchmark.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu_a100
+#SBATCH --gpus=1
+#SBATCH --job-name=erwin
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=20:00:00
+#SBATCH --output=slurm_output_%A.out
+
+module purge
+module load 2024
+module load Anaconda3/2024.06-1
+module load 2023
+module load CUDA/12.4.0
+
+source activate erwin
+
+cd $HOME/erwinpp/benchmarks
+
+srun python velocity_checker.py
