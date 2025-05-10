ffe7ad5b33198763a967b55e6d8dd81102b482c3
pedrocurvo <pedro.mpcurvo@gmail.com>
2025-05-10 21:04:25 +0200

    Add SLURM job scripts for evaluation and training of the Transolver model
    - Introduced `Evaluation.sh` and `Training.sh` scripts to facilitate model evaluation and training using SLURM.
    - Both scripts include necessary environment setup, module loading, and job specifications for GPU usage.
    - Configured to run the `main_evaluation.py` and `main.py` scripts with specified data directories and output paths.

---
+++ 02-Car-Design-ShapeNetCar/Training.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu_a100
+#SBATCH --gpus=1
+#SBATCH --job-name=erwin
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=20:00:00
+#SBATCH --output=slurm_output/slurm_output_%A.out
+
+module purge
+module load 2024
+module load Anaconda3/2024.06-1
+module load 2023
+module load CUDA/12.4.0
+
+
+cd $HOME/erwinpp/02-Car-Design-ShapeNetCar
+
+srun env CUDA_LAUNCH_BLOCKING=1 python main.py --cfd_model=Transolver --data_dir shapenet_car/mlcfd_data/training_data --save_dir shapenet_car/mlcfd_data/preprocessed_data   
